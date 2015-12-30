7b3091de14c5adf29c89eb47bde4fc6de1d8d317
trbarron <tdawgg2@yahoo.com>
2015-12-30 12:27:47 -0800

    Create HueLights.bat

---
+++ HueLights.bat
@@ -0,0 +1,10 @@
+setlocal EnableDelayedExpansion
+curl http://w1.weather.gov/xml/current_obs/KBFI.xml>File.xml
+for /F "delims=" %%a in ('findstr /i "temp_f" file.xml') do set temp=%%a
+set /a temp=!temp:~9,-11!
+set /a temp=temp*3+98
+for /F "delims=" %%a in ('findstr /i "<weather>" file.xml') do set weather=%%a
+set weather=%weather:~10,-10%
+IF "%weather%"=="Partly Cloudy" (set wea_hue=12200)
+IF "%weather%"=="Rainy" (set wea_hue=40000)
+hue.bat -k e46690a2ae054971ed4497633b327b3 -l "1 2 3" -on true -l "2" -c %temp% -h %wea_hue% -s 255 -l "1 3" -h 1 -s 1 -c %temp%
